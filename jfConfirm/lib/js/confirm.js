ea23509018ff70b0c56f154d54b2176fe3eaf41d
Jefferson <jeffersonmiranda0@gmail.com>
2016-04-18 22:58:08 -0300

    jfConfirm
    Primary Update

---
+++ jfConfirm/lib/js/confirm.js
@@ -0,0 +1,42 @@
+/*! jfConfirm 0.0.1 | (c) Jefferson Miranda | www.jeffersonmiranda.com.br */
+
+$(document).ready(function(){
+
+	var table = function(input){
+		var _text = "";
+		for(j in input){
+			if(input[j].value != undefined){
+				_text += "<tr>";
+					_text += "<td>"+ input[j].id +"</td>";
+					_text += "<td>"+ input[j].value +"</td>";
+				_text += "</tr>";
+			}
+		}
+		return _text;
+	}
+
+	var layout = function(mensagem, input){
+		$("body").append("<div id='confirmShadow'><div id='confirm'><h1>"+mensagem+"<h1/><p>Confirme a baixo as informações:</p><table>" + table(input) +"</table><div><a href='#' class='option'>Sim</a><a href='#' class='option'>Não</a></div></div></div>");
+
+		$(".option")[0].focus();
+
+		$(".option").click(function(){
+			if($(this).text() === "Sim"){
+				$("#formSuccess").submit();
+			} else {
+				input[0].focus();
+				$("#confirmShadow").remove();
+				$("#confirm").remove();
+			}
+		});
+	}
+	
+	var confirma = function(){
+		var input 		= $("input");
+		layout("Deseja realmente salvar?", input);
+	};
+
+	$(".btn").click(function(){
+		confirma();
+	});
+});
